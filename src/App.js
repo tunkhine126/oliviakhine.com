13d55d6897aee53dbcd894a731f00375e1a9f94d
Tun Khine <tunkhine126@gmail.com>
2023-06-20 02:50:13 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import sunflower from './assets/images/sunflowerfield.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={sunflower} className="sunflower" alt="sunflower" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Olivia's sunflower field
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
